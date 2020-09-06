3f17cbfae5682d8b6325e98c4536218cfd996319
Ahad Chowdhury <ahad@maacpiash.com>
2020-09-07 01:51:23 +1000

    Add scraping logic to scraper package

---
+++ packages/scraper/src/helpers.ts
@@ -0,0 +1,40 @@
+/*
+ * Prospect: A blog reader app
+ * Copyright (c) 2020 Mohammad Abdul Ahad Chowdhury
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+import axios from 'axios'
+import { load } from 'cheerio'
+
+export default async function getPageParsed(
+  url: string,
+  selector: string,
+): Promise<string[]> {
+  const { data } = await axios(url)
+  const $ = load(data) as CheerioStatic
+  const links: (string | undefined)[] = []
+  $(selector).each((_, link) => links.push($(link).attr('href')))
+  return links.filter(clear)
+}
+
+// https://stackoverflow.com/questions/43118692
+function clear<TValue>(value: TValue | null | undefined): value is TValue {
+  return value !== null && value !== undefined
+}
