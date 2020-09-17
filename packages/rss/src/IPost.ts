fdea852a425f3ca8730e2ed2649bcf1e10b092c2
Ahad Chowdhury <ahad@maacpiash.com>
2020-09-17 22:12:30 +1000

    Change license from MIT to AGPL-3.0
    Because of the lack of any copyleft clause in the MIT license, anyone
    can obtain a copy of the software, modify it, and make it closed source,
    which I, personally, would absolutely hate to see.
    
    To prevent this, the license has been changed to the good-old AGPL-3.0,
    a license that is quite permissive and yet a copyleft one: you are free
    to get a copy of this software, use it, modify it, but you must extend
    the same freedom over your copy to everyone else.

--- packages/rss/src/IPost.ts
+++ packages/rss/src/IPost.ts
@@ -1,3 +1,19 @@
+/*
+ * Prospect: Read the latest posts from popular coding blogs at one place
+ * Copyright (C) 2020  Mohammad Abdul Ahad Chowdhury
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 export default interface IPost {
   title: string
   link: string
